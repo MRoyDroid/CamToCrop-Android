7e6d93563034e685871954948e5a167e8538b09a
Mithu Roy <mithu.roy@codelogicx.com>
2018-06-08 12:33:49 +0530

    Makes the sample more interactive

--- app/src/main/java/com/mithuroy/camcrop/MainActivity.kt
+++ app/src/main/java/com/mithuroy/camcrop/MainActivity.kt
@@ -2,19 +2,24 @@ package com.mithuroy.camcrop
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import com.mithuroy.camtocrop.CameraActivity
 import com.mithuroy.camtocrop.CameraActivity.Companion.IMAGE_PATH
 import com.mithuroy.camtocrop.CameraActivity.Companion.REQUEST_IMAGE_PATH
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        CameraActivity().start(this)
-        startFragment()
+
+        btnTakePhoto.setOnClickListener { CameraActivity().start(this) }
+
+//        startFragment()
     }
 
     private fun startFragment() {
@@ -27,7 +32,15 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && data != null) {
             when (requestCode) {
-                REQUEST_IMAGE_PATH -> Log.d("FilePath", data.getStringExtra(IMAGE_PATH))
+                REQUEST_IMAGE_PATH -> imageView.setImageURI(
+                    Uri.fromFile(
+                        File(
+                            data.getStringExtra(
+                                IMAGE_PATH
+                            )
+                        )
+                    )
+                )
             }
         }
     }
